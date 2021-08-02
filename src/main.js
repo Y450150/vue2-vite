0d36b9da3024ebc517674f8e7ae10257f970e571
v_yvywang <v_yvywang@tencent.com>
2021-08-02 16:51:47 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -2,12 +2,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
-import VueRouter from 'vue-router'
-import dir from './router/zhichu.js'
 import { createRouter } from './router.js'
 import './core/antd'
 Vue.prototype.$axios = axios
-console.log(dir)
 export function createApp() {
   const router = createRouter()
   const app = new Vue({
