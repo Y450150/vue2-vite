c6c4992b8392db49184068ce0e510e5cc273b83d
v_yvywang <v_yvywang@tencent.com>
2021-12-02 18:06:12 +0800

    merge

--- node/app.ts
+++ node/app.ts
@@ -21,24 +21,8 @@ server.use(CORS);
 server.use("/dist", serve("./dist", true)); // 部署dist目录
 server.use("/api", router); // 部署路由
 
-MiddleWaresRouter(server); // 放在这里，让前面的先命中
+MiddleWaresRouter(server); // 放在后面，让前面的先命中
+
 export { server };
 
-// var ws = require("nodejs-websocket");
-// const mockPath = path.resolve(__dirname, "./mock/chat.json");
-// const clientList = [];
 
-// ws.createServer(function (conn) {
-//   console.log("New connection");
-//   clientList.push(conn);
-//   conn.on("text", function (str) {
-//     var data = JSON.parse(fs.readFileSync(mockPath, "utf-8"));
-//     data.push(JSON.parse(str));
-//     data = JSON.stringify(data);
-//     fs.writeFileSync(mockPath, data);
-//     clientList.forEach((conn) => conn.send(data));
-//   });
-//   conn.on("close", function (code, reason) {
-//     console.log("Connection closed");
-//   });
-// }).listen(8888);
